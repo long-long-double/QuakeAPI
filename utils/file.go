06be3d5dbcc0104e6eb11e60086ac27cbbad975b
EmYiQing <2023503307@qq.com>
2021-02-17 11:47:00 +0800

    细节问题

--- utils/file.go
+++ utils/file.go
@@ -39,21 +39,20 @@ func WriteOutput(content string, filename string) {
 			log.Log("Delete Output File Error:"+err.Error(), log.ERROR)
 			return
 		}
-	} else {
-		log.Log("Create Output File", log.INFO)
-		file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0777)
-		if file != nil {
-			defer file.Close()
-		}
-		if err != nil {
-			log.Log("Create File Error:"+err.Error(), log.ERROR)
-			return
-		}
-		_, err = file.Write([]byte(content))
-		if err != nil {
-			log.Log("Write File Error:"+err.Error(), log.ERROR)
-			return
-		}
+	}
+	log.Log("Create Output File", log.INFO)
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0777)
+	if file != nil {
+		defer file.Close()
+	}
+	if err != nil {
+		log.Log("Create File Error:"+err.Error(), log.ERROR)
+		return
+	}
+	_, err = file.Write([]byte(content))
+	if err != nil {
+		log.Log("Write File Error:"+err.Error(), log.ERROR)
+		return
 	}
 }
 
